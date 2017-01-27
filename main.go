b6c028dc0d6b3dbe45c00eb9cae2624e76af4211
Jumpei Ogawa <phanect@users.noreply.github.com>
2017-01-27 19:14:33 +0900

    tmp

--- main.go
+++ main.go
@@ -40,6 +40,66 @@ func main() {
     default:
       help(true)
   }
+
+
+
+
+
+
+
+  //
+  // var optvalArgs []string = os.Args[1:] // First argument is skipped because it is program name
+  // /** All arguments given to the command. (== $@) */
+  // args := []string{} // = strings.Split(optvalArgs[len(optvalArgs)-1], " ") // Last arg is arguments to analyze
+  // /** Option names the user wants to get value */
+  // optNames := []string{} // = optvalArgs[:len(optvalArgs)-1] // Option names; e.g. --foo, -b
+  //
+  // //
+  // // Analyze given arguments to optval
+  // //
+  // for i := 0; i < len(optvalArgs); i++ {
+  //   if optvalArgs[i] == "--" { // when reached to --
+  //     for j := i + 1; j < len(optvalArgs); j++ {
+  //       args = append(args, optvalArgs[j])
+  //     }
+  //
+  //     break
+  //   }
+  //
+  //   if strings.HasPrefix(optvalArgs[i], "-") {
+  //     optNames = append(optNames, optvalArgs[i])
+  //   } else {
+  //     fmt.Fprintln(os.Stderr, "Error: '%[1]d' is not option; options have to starts with - or --. Also check if you quote args to analyze: optval --foo \"$@\"", args[i])
+  //     os.Exit(1)
+  //   }
+  // }
+  //
+  // //
+  // // Analyze options
+  // //
+  // for i := 0; i < len(args); i++ {
+  //   var _optName, value string
+  //
+  //   if (!strings.HasPrefix(args[i], "-")) { // if not option
+  //     continue
+  //   } else if (strings.Contains(args[i], "=")) { // --foo=bar style option
+  //     var tmp = strings.Split(args[i], "=")
+  //     _optName = tmp[0]
+  //     value = tmp[1]
+  //   } else { // --foo bar style option
+  //     _optName = args[i]
+  //     value = args[i+1]
+  //
+  //     i++ // Skip next arg (== the option's value)
+  //   }
+  //
+  //   for _, optName := range(optNames) {
+  //     if optName == _optName {
+  //       os.Stdout.WriteString(value)
+  //       os.Exit(0)
+  //     }
+  //   }
+  // }
 }
 
 func initialize(cliArgs []string) {
